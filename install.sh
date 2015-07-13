a12357f7e87f6fddf151b942e16178282df030da
mevitan <td.anh0812@gmail.com>
2015-07-13 14:28:26 +0700

    Update install script

--- install.sh
+++ install.sh
@@ -4,10 +4,17 @@ cp ~/.vimrc ~/.vimrc.bak
 echo 'Installing new vimrc ...'
 cd ~/.nvim_runtime
 
-echo 'set runtimepath+=~/.nvim_runtime
+echo 'set runtimepath+=~/.vim_runtime
 
-source ~/.nvim_runtime/vimrcs/basic.vim
-source ~/.nvim_runtime/vimrcs/plugins_config.vim
+try
+source ~/.vim_runtime/vimrcs/my_configs.vim
+catch
+endtry
+
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
 
 set nocp
 set nu
