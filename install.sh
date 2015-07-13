101f79598741725677438236edbe1fe4e5700bc7
mevitan <td.anh0812@gmail.com>
2015-07-13 15:30:57 +0700

    add try-catch for personal config from user

--- install.sh
+++ install.sh
@@ -6,12 +6,14 @@ cd ~/.nvim_runtime
 
 echo 'set runtimepath+=~/.nvim_runtime
 
+try
+    source ~/.nvim_runtime/vimrcs/custom.vim
+catch
+endtry
+
 source ~/.nvim_runtime/vimrcs/basic.vim
 source ~/.nvim_runtime/vimrcs/plugins_config.vim
 source ~/.nvim_runtime/vimrcs/filetype.vim
-
-set nocp
-set nu
-inoremap ii <ESC>' > ~/.vimrc
+' > ~/.vimrc
 
 echo "Installed succesfully! :D"
