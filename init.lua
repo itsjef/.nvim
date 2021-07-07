bfd6dbbf2a0b20d1713d32326f89342362b93190
Adrian_Cin <adrian@cinnamon.is>
2021-07-08 00:39:44 +0700

    Stop ignoring init.lua

---
+++ init.lua
@@ -0,0 +1,9 @@
+vim.g.python_host_prog = '/home/adrian/.pyenv/versions/pynvim/bin/python'
+vim.g.python3_host_prog = '/home/adrian/.pyenv/versions/pynvim3/bin/python'
+
+-- do not reorder
+require('globals')
+require('general')
+require('keymappings')
+require('plugins')
+require('themes')
---
+++ lua/plugins/init.lua
@@ -0,0 +1,16 @@
+require('plugins.plug')
+
+-- LSPs
+require('plugins.lsp')
+
+-- Other plugins
+require('plugins.ale')
+require('plugins.bufferline')
+require('plugins.compe')
+require('plugins.easyalign')
+require('plugins.indent-blankline')
+require('plugins.lualine')
+require('plugins.nvim-tree')
+require('plugins.rainbow')
+require('plugins.telescope')
+require('plugins.treesitter')
