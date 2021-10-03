9ef5323bcdf4c9088b0833fe90358ba78ac81a1f
Adrian_Cin <adrian@cinnamon.is>
2021-10-04 03:20:27 +0700

    Update nvim-tree config

--- lua/conf/nvim-tree.lua
+++ lua/conf/nvim-tree.lua
@@ -1,23 +1,21 @@
 local g = vim.g
 
-g.nvim_tree_side = 'right'
-g.nvim_tree_width = 35
-g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
-g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 1
-g.nvim_tree_quit_on_open = 0
-g.nvim_tree_follow = 1
+g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '__pycache__'}
+g.nvim_tree_gitignore = 1
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_root_folder_modifier = ':t'
-g.nvim_tree_tab_open = 0
-g.nvim_tree_allow_resize = 1
-
 g.nvim_tree_show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1
+  git = 1,
+  folders = 1,
+  files = 1,
+  folder_arrows = 1,
+}
+
+require'nvim-tree'.setup {
+  view = {
+    side = 'right',
+  }
 }
 
 vim.api.nvim_set_keymap('n', '<leader>nn', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
