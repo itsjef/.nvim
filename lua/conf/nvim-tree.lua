ffb8d32db8b1b04762a98044f73ca3d1135244de
Anh Tran (Adrian) <adrian@cinnamon.is>
2022-06-13 15:41:05 +0700

    Upgrade nvim-tree config

--- lua/conf/nvim-tree.lua
+++ lua/conf/nvim-tree.lua
@@ -1,20 +1,9 @@
-local g = vim.g
-
-g.nvim_tree_git_hl = 1
-g.nvim_tree_root_folder_modifier = ':t'
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1,
-  folder_arrows = 1,
-}
-
 require'nvim-tree'.setup {
   view = {
     side = 'right',
   },
-  git = {
-    ignore = true
+  renderer = {
+    highlight_git = true,
   }
 }
 
