0c126eca8a1d2ba22e487f64fc08caf9ca2432e5
Anh Tran (Adrian) <adrian@cinnamon.is>
2022-08-11 18:30:00 +0700

    Format lua/plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,28 +1,34 @@
 -- Bootstrapping, ensure that packer.nvim is installed
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
   vim.cmd [[packadd packer.nvim]]
 end
 
 -- Automatically run :PackerCompile whenever this file is updated
-local plugins_path = fn.stdpath('config')..'/lua/plugins.lua'
-vim.cmd('autocmd BufWritePost '..plugins_path..' PackerCompile')
+local plugins_path = fn.stdpath('config') .. '/lua/plugins.lua'
+vim.cmd('autocmd BufWritePost ' .. plugins_path .. ' PackerCompile')
 
 -- Install plugins
-require('packer').startup({function()
+require('packer').startup({ function()
   -- Packer can manage itself
-  use {'wbthomason/packer.nvim'}
+  use { 'wbthomason/packer.nvim' }
 
-  -- diagnostic, code actions, and more.
-  use({ "jose-elias-alvarez/null-ls.nvim",
+  -- LSPs: diagnostic, auto-formatting, code actions, and more.
+  use({ 'jose-elias-alvarez/null-ls.nvim',
     requires = {
-      "nvim-lua/plenary.nvim",
-      "neovim/nvim-lspconfig"
+      'nvim-lua/plenary.nvim',
+      'neovim/nvim-lspconfig'
     }
   })
+  use { 'williamboman/mason-lspconfig.nvim',
+    requires = {
+      'williamboman/mason.nvim',
+      'neovim/nvim-lspconfig'
+    }
+  }
 
   -- File explorer
   use {
@@ -33,18 +39,10 @@ require('packer').startup({function()
   }
 
   -- Git
-  use {'tpope/vim-fugitive'}
-
-  -- LSP
-  use { 'williamboman/mason-lspconfig.nvim',
-    requires = {
-      'williamboman/mason.nvim',
-      'neovim/nvim-lspconfig'
-    }
-  }
+  use { 'tpope/vim-fugitive' }
 
   -- Autocompletion & snippets
-  use {'mattn/emmet-vim'}
+  use { 'mattn/emmet-vim' }
   use {
     'hrsh7th/nvim-cmp',
     requires = {
@@ -56,46 +54,46 @@ require('packer').startup({function()
   }
   use {
     'L3MON4D3/LuaSnip',
-    requires = {'rafamadriz/friendly-snippets'},
+    requires = { 'rafamadriz/friendly-snippets' },
     config = function()
-      require'luasnip.loaders.from_vscode'.load()
+      require 'luasnip.loaders.from_vscode'.load()
     end
   }
 
   -- Colorscheme & Syntax Highlighting
-  use {'catppuccin/nvim', as = 'catppuccin'}
-  use {'ful1e5/onedark.nvim'}
-  use {'marko-cerovac/material.nvim'}
-  use {'projekt0n/github-nvim-theme'}
+  use { 'catppuccin/nvim', as = 'catppuccin' }
+  use { 'ful1e5/onedark.nvim' }
+  use { 'marko-cerovac/material.nvim' }
+  use { 'projekt0n/github-nvim-theme' }
   use {
     'akinsho/bufferline.nvim',
-    requires = {'kyazdani42/nvim-web-devicons'},
+    requires = { 'kyazdani42/nvim-web-devicons' },
     config = function()
-      require('bufferline').setup{}
+      require('bufferline').setup {}
     end
   }
-  use {'nvim-lualine/lualine.nvim'}
-  use {'kshenoy/vim-signature'}
+  use { 'nvim-lualine/lualine.nvim' }
+  use { 'kshenoy/vim-signature' }
   use {
     'lewis6991/gitsigns.nvim',
-    requires = {'nvim-lua/plenary.nvim'},
+    requires = { 'nvim-lua/plenary.nvim' },
     config = function()
-      require('gitsigns').setup{}
+      require('gitsigns').setup {}
     end
   }
   use {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
-      vim.g.indent_blankline_filetype_exclude = {'help', 'nerdtree', 'defx'}
+      vim.g.indent_blankline_filetype_exclude = { 'help', 'nerdtree', 'defx' }
       vim.g.indent_blankline_use_treesitter = true
       -- vim.g.indent_blankline_show_current_context = true
     end
   }
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-  use {'nvim-treesitter/nvim-treesitter-textobjects' }
-  use {'nvim-treesitter/nvim-treesitter-context'}
-  use {'p00f/nvim-ts-rainbow'}
-  use {'sheerun/vim-polyglot'}
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
+  use { 'nvim-treesitter/nvim-treesitter-context' }
+  use { 'p00f/nvim-ts-rainbow' }
+  use { 'sheerun/vim-polyglot' }
   -- use {'onsails/lspkind-nvim'}
 
   -- Navigation
@@ -105,33 +103,33 @@ require('packer').startup({function()
   }
   use {
     'nvim-telescope/telescope.nvim',
-    requires = {'nvim-lua/plenary.nvim'}
+    requires = { 'nvim-lua/plenary.nvim' }
   }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   -- Utils & Helpers
   use {
     'junegunn/vim-easy-align',
     config = function()
       -- Start interactive EasyAlign in visual mode (e.g. vipga)
-      vim.keymap.set('x', 'ga', '<Plug>(EasyAlign)', {silent = true})
+      vim.keymap.set('x', 'ga', '<Plug>(EasyAlign)', { silent = true })
       -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
-      vim.keymap.set('n', 'ga', '<Plug>(EasyAlign)', {silent = true})
+      vim.keymap.set('n', 'ga', '<Plug>(EasyAlign)', { silent = true })
     end
   }
   use {
-    "windwp/nvim-autopairs",
+    'windwp/nvim-autopairs',
     config = function()
-      require("nvim-autopairs").setup {}
+      require('nvim-autopairs').setup {}
     end
   }
   use {
-    "kylechui/nvim-surround",
+    'kylechui/nvim-surround',
     config = function()
       require('nvim-surround').setup {}
     end
   }
-  use {'tpope/vim-commentary'}
+  use { 'tpope/vim-commentary' }
   use {
     'luukvbaal/stabilize.nvim',
     config = function()
