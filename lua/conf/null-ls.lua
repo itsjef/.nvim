14911e5626424743ef711dd05051286d93ec937d
Anh Tran (Adrian) <adrian@cinnamon.is>
2022-08-11 12:51:54 +0700

    Refactor some null-ls config

--- lua/conf/null-ls.lua
+++ lua/conf/null-ls.lua
@@ -6,11 +6,25 @@ local sources = {
   builtins.formatting.trim_newlines,
   builtins.formatting.trim_whitespace,
 }
-local on_attach = function(client)
-  if client.resolved_capabilities.document_formatting then
-    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+        vim.lsp.buf.formatting_sync()
+      end,
+    })
   end
 end
+-- local on_attach = function(client)
+--   if client.resolved_capabilities.document_formatting then
+--     vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+--   end
+-- end
 
 nls.setup({
   sources = sources,
