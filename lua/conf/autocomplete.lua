ead89797158abf56c6dd3ecf011d19dd4e44b64e
Anh Tran (Adrian) <adrian@cinnamon.is>
2023-10-04 14:37:08 +0700

    tiny update autocomplete mapping

--- lua/conf/autocomplete.lua
+++ lua/conf/autocomplete.lua
@@ -1,27 +1,23 @@
 vim.o.completeopt = 'menuone,noselect'
 
 -- nvim-cmp setup
-local fn = vim.fn
 local luasnip = require 'luasnip'
 local cmp = require 'cmp'
 local lspkind = require 'lspkind'
 
-local function t(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_backspace = function()
-  local col = fn.col('.') - 1
-  return col == 0 or fn.getline('.'):sub(col, col):match('%s') ~= nil
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local function tab(fallback)
   if cmp.visible() then
     cmp.select_next_item()
   elseif luasnip.expand_or_jumpable() then
-    fn.feedkeys(t('<Plug>luasnip-expand-or-jump'), '')
-  elseif check_backspace() then
-    fn.feedkeys(t('<Tab>'), 'n')
+    luasnip.expand_or_jump()
+  elseif has_words_before() then
+    cmp.complete()
   else
     fallback()
   end
@@ -31,7 +27,7 @@ local function shift_tab(fallback)
   if cmp.visible() then
     cmp.select_prev_item()
   elseif luasnip.jumpable(-1) then
-    fn.feedkeys(t('<Plug>luasnip-jump-prev'), '')
+    luasnip.jump(-1)
   else
     fallback()
   end
