e431da9ed17c7f97ed2000d2a3edacdf4ccf880d
Anh Tran (Adrian) <adrian@cinnamon.is>
2023-12-08 18:22:53 +0700

    upgrade LuaSnip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -70,6 +70,8 @@ return {
   },
   {
     'L3MON4D3/LuaSnip',
+    version = 'v2.*',
+    build = 'make install_jsregexp',
     dependencies = { 'rafamadriz/friendly-snippets' },
     config = function()
       require('luasnip.loaders.from_vscode').load()
