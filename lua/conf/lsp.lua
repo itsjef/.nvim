128db162c722f0b2f560baf601170d272f7ba459
Anh Tran (Adrian) <adrian@cinnamon.is>
2024-07-02 16:04:38 +0700

    update lsp

--- lua/conf/lsp.lua
+++ lua/conf/lsp.lua
@@ -3,9 +3,10 @@ vim.diagnostic.config({ float = { border = border } })
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+-- replaced with mini.bracketed
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
 local on_attach = function(_, bufnr)
   --Enable completion triggered by <c-x><c-o>
@@ -15,9 +16,9 @@ local on_attach = function(_, bufnr)
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
@@ -27,67 +28,75 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>=', vim.lsp.buf.formatting, bufopts)
-end
-
-local on_attach_no_formatting = function(client, bufnr)
-  client.server_capabilities.document_formatting = false
-  client.server_capabilities.document_range_formatting = false
-  on_attach(client, bufnr)
+  -- vim.keymap.set('n', '<leader>=', vim.lsp.buf.formatting, bufopts)
 end
 
 local mason = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
 local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local handle_lsp_border = {
+  ["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover, { border = border }
+  ),
+  ["textDocument/signatureHelp"] = vim.lsp.with(
+    vim.lsp.handlers.signature_help, { border = border }
+  ),
+}
 
 local default_handler = function(server_name)
   lspconfig[server_name].setup {
     on_attach = on_attach,
-    handlers = {
-      ["textDocument/hover"] = vim.lsp.with(
-        vim.lsp.handlers.hover, { border = border }
-      ),
-      ["textDocument/signatureHelp"] = vim.lsp.with(
-        vim.lsp.handlers.signature_help, { border = border }
-      ),
-    }
+    capabilities = capabilities,
+    handlers = handle_lsp_border,
   }
 end
 
-local server_handlers = {
+local handlers = {
   default_handler,
 
-  ['dockerls'] = function()
-    lspconfig['dockerls'].setup {
-      on_attach = on_attach_no_formatting,
-    }
-  end,
-
-  ['lua_ls'] = function()
-    lspconfig['lua_ls'].setup {
-      on_attach = on_attach_no_formatting,
+  ['basedpyright'] = function()
+    lspconfig['basedpyright'].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      handlers = handle_lsp_border,
       settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim", "use" }
-          }
-        }
-      }
+        basedpyright = {
+          disableOrganizeImports = true,
+          typeCheckingMode = 'strict',
+          -- analysis = {
+          --   ignore = { '*' }, -- Ignore all files for analysis to exclusively use Ruff for linting
+          -- },
+        },
+      },
     }
   end,
 
-  ['marksman'] = nil,
-  ['pyright'] = nil,
-  ['yamlls'] = nil,
+  ['ruff_lsp'] = function()
+    lspconfig['ruff_lsp'].setup {
+      on_attach = function(client, bufnr)
+        client.server_capabilities.hoverProvider = false
+        on_attach(client, bufnr)
+      end,
+      capabilities = capabilities,
+      handlers = handle_lsp_border,
+    }
+  end,
+
+  ['docker_ls'] = nil,
+  ['lua_ls'] = nil,
 }
 
 local ensure_installed = {}
-for server, _ in ipairs(server_handlers) do
+for server, _ in ipairs(handlers) do
   if type(server) == "string" then
     table.insert(ensure_installed, server)
   end
 end
 
 mason.setup()
-mason_lspconfig.setup({ ensure_installed = ensure_installed })
-mason_lspconfig.setup_handlers(server_handlers)
+mason_lspconfig.setup({
+  ensure_installed = ensure_installed,
+  handlers = handlers
+})
