0390d76447626772eb5ab9a4ad604cfded96bafa
Anh Tran (Adrian) <adrian@cinnamon.is>
2024-07-03 11:18:59 +0700

    update mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,7 +4,22 @@ return {
     local api = vim.api
 
     require('mini.ai').setup()
-    require('mini.bracketed').setup()
+    require('mini.bracketed').setup({
+      buffer     = { suffix = 'b', options = {} },
+      comment    = { suffix = 'c', options = {} },
+      conflict   = { suffix = 'x', options = {} },
+      -- diagnostic = { suffix = 'd', options = {} },  -- default in Neovim 0.10
+      file       = { suffix = 'f', options = {} },
+      indent     = { suffix = 'i', options = {} },
+      jump       = { suffix = 'j', options = {} },
+      location   = { suffix = 'l', options = {} },
+      oldfile    = { suffix = 'o', options = {} },
+      quickfix   = { suffix = 'q', options = {} },
+      treesitter = { suffix = 't', options = {} },
+      undo       = { suffix = 'u', options = {} },
+      window     = { suffix = 'w', options = {} },
+      yank       = { suffix = 'y', options = {} },
+    })
     require('mini.bufremove').setup()
     require('mini.comment').setup()
     require('mini.trailspace').setup()
