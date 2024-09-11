3814d71dfff94404c552347dabf37383de6012af
Anh Tran (Adrian) <adrian@cinnamon.is>
2024-09-11 14:48:59 +0700

    distingush border between splits

--- lua/conf/themes.lua
+++ lua/conf/themes.lua
@@ -1,4 +1,12 @@
-require('catppuccin').setup()
+require('catppuccin').setup({
+  custom_highlights = function(colors)
+    return {
+      WinSeparator = {
+        bg = colors.surface0,
+      },
+    }
+  end
+})
 require('github-theme').setup()
 require('material').setup()
 require('onedark').setup()
